6af033cffae5fbd4c8cf66ee8d1d51578c542ba8
revita <revitasurya@gmail.com>
2023-09-03 16:47:59 +0700

    add make appointment

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'igcg6z',
+  projectId: "igcg6z",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+  defaultCommandTimeout: 30000,
+  pageLoadTimeout: 60000,
+  requestTimeout: 60000,
+  viewportWidth: 1536,
+  viewportHeight: 960,
+  video: false,
+  retries: {
+    runMode: 2,
   },
 });
--- cypress/e2e/demo-web/appointment.cy.js
+++ cypress/e2e/demo-web/appointment.cy.js
@@ -2,22 +2,45 @@
 //declaration
 
 const web = "https://katalon-demo-cura.herokuapp.com/";
-const username = "John Doe";
-const password = "ThisIsNotAPassword";
+
+//function
+const doLogin = () => {
+  cy.fixture("account").then((account) => {
+    cy.get("#btn-make-appointment").click();
+    cy.get("#txt-username").type(`${account.username}`);
+    cy.get("#txt-password").type(`${account.password}`);
+    cy.get("#btn-login").click();
+
+    //assert
+    cy.get("h2").should("contain.text", "Make Appointment");
+    cy.url().should(
+      "eq",
+      "https://katalon-demo-cura.herokuapp.com/#appointment"
+    );
+  });
+};
 
 //main function
 describe("Katalon Demo App", () => {
   beforeEach(() => {
     cy.visit(`${web}`);
+    doLogin();
   });
 
-  it("Do login", () => {
-    cy.get("#btn-make-appointment").click();
-    cy.get("#txt-username").type(`${username}`);
-    cy.get("#txt-password").type(`${password}`);
-    cy.get("#btn-login").click();
+  it("Make appointment", () => {
+    cy.get("#combo_facility").select("Tokyo CURA Healthcare Center");
+    cy.get("#chk_hospotal_readmission").click();
+    cy.get("#txt_visit_date").type("27/09/2023").click();
+    cy.get("#radio_program_medicaid").check("Medicaid");
+    cy.get("#txt_comment").type("testing");
+    cy.contains("Book Appointment").click();
 
     //assert
-    cy.get("h2").should("contain.text", "Make Appointment");
+    cy.contains("Appointment Confirmation").should("exist");
+    cy.get("#facility").should("include.text", "Tokyo CURA Healthcare Center");
+    cy.get("#hospital_readmission").should("include.text", "Yes");
+    cy.get("#program").should("include.text", "Medicaid");
+    cy.get("#visit_date").should("include.text", "27/09/2023");
+    cy.get("#comment").should("include.text", "testing");
   });
 });
